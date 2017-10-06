7573e257c3816f6aff9b9997774aa0259069808d
= <=>
2017-10-06 10:03:33 +0100

    User Registration and Signup Completed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                   Project Management System Application
                 </div>
 
                 <div class="links">
