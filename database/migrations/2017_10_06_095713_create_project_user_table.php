66b56bc5cafdafa001825e525adf60780c1af528
= <=>
2017-10-06 11:13:38 +0100

    project_user and task_user are both many to many relationship .Both has been completed successfully and migrated in the database succesfully

--- database/migrations/2017_10_06_095713_create_project_user_table.php
+++ database/migrations/2017_10_06_095713_create_project_user_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTasksTable extends Migration
+class CreateProjectUserTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,21 +13,15 @@ class CreateTasksTable extends Migration
      */
     public function up()
     {
-        Schema::create('tasks', function (Blueprint $table) {
+        Schema::create('project_user', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
             $table->integer('project_id')->unsigned();
             $table->integer('user_id')->unsigned();
-            $table->integer('company_id')->unsigned();
-            $table->integer('days')->unsigned()->unsigned();
-            $table->integer('hours')->unsigned()->nullable();
-
             $table->timestamps();
 
-            /*--User ,Project  and company foreign Key--*/
+            /*--- Foreign Keys --*/
             $table->foreign('user_id')->references('id')->on('users');
             $table->foreign('project_id')->references('id')->on('projects');
-            $table->foreign('company_id')->references('id')->on('companies');
         });
     }
 
@@ -38,6 +32,6 @@ class CreateTasksTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tasks');
+        Schema::dropIfExists('project_user');
     }
 }
